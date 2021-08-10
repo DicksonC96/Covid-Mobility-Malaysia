25294d4794b4c6536d23944d1861d4349cdb4429
DicksonC96 <dicksonc96@hotmail.com>
2021-08-11 01:17:16 +0800

    Fixed script to only auto-update data

--- data-updater.sh
+++ data-updater.sh
@@ -3,7 +3,8 @@
 cd ~/Covid-Mobility-Malaysia
 ./script-compiled.py >> log/$(date +%Y%m%d).log
 if [[ "$(git status --porcelain)" != "" ]]; then
-	git commit -m "Daily data update" -a
+	git add data/*
+	git commit -m "Daily data update"
 	git tag -a `date "+%Y%m%d"` -m "Daily data update"
 	git push origin master --tags
 fi
